16d9f60b539b28b1f17eae6d50a67ea39a132c86
Ezhil Bharghavi <ebpkdk@gmail.com>
2026-02-15 15:45:36 +0530

    Day 2 - Find Median Sorted Array[4]

---
+++ Arrays/findMedianSortedArray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        nums=nums1+nums2
+        nums.sort()
+        k=len(nums)
+        if k%2==1:
+            return nums[k//2]
+        return (nums[k//2] + nums[k//2 - 1])/2.0
