06f8903dbd6efcf74d9627bd1cf13d62c6d138de
EBharghavi <ebpkdk@gmail.com>
2026-03-01 15:39:46 +0530

    Day3 - [7] Reverse Integer

---
+++ Math/ReverseInterger.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        long temp = 0;
+        while(x!=0)// for postive and negative condition
+        {
+            int rem = x % 10;
+           
+            temp = (temp*10) +rem;
+            x=x/10;
+        }
+         if ( temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE) // any decimal value can return under 32 bit can pass 
+            {
+                return 0;
+            }
+        return (int)temp;
+    }
+}
