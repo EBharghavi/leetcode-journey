ae83270021df198278f22978a93acb830c357697
EBharghavi <ebpkdk@gmail.com>
2026-08-01 22:06:01 +0530

    Day8 - [181]EmployeesEarningMoreThanManagers

---
+++ DatabaseEmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,12 @@
+
+SELECT 
+    e.name AS Employee
+FROM 
+    Employee e
+WHERE 
+    EXISTS (
+        SELECT 1 
+        FROM Employee m 
+        WHERE m.id = e.managerId 
+        AND e.salary > m.salary
+    );
