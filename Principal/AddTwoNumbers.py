0fda16bbc25fc9ca0356a7b77068d8e5446046d9
EBharghavi <ebpkdk@gmail.com>
2026-08-26 11:09:41 +0530

    [2]Add Two Numbers

---
+++ Principal/AddTwoNumbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
